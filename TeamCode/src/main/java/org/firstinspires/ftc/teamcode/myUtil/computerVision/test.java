2b268a87dc3ad70fc47e2a249d1f06dd88f25d04
FTC Team 20325 Maximum Resistance <93337173+FTC20325MaximumResistance@users.noreply.github.com>
2024-09-17 19:14:14 -0500

    templatefied by Cooper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/myUtil/computerVision/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/myUtil/computerVision/test.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.myUtil.computerVision;
 
-import com.acmerobotics.dashboard.config.Config;
+//import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -12,7 +12,7 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvWebcam;
 //@Disabled
-@Config
+//@Config
 @TeleOp(name="VisionTest")
 public class test extends OpMode {
     public static compVis.Colors color = compVis.Colors.RED;
