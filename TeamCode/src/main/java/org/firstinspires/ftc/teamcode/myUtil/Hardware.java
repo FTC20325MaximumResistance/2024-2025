c549f102bd1cd6c8d68c984d25c0f6d330c3f916
FTC Team 20325 Maximum Resistance <93337173+FTC20325MaximumResistance@users.noreply.github.com>
2024-09-25 17:06:38 -0500

    templatefied by Cooper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/myUtil/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/myUtil/Hardware.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.myUtil;
 
-
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
